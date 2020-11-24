3ee79a569c7de03246ac0aa62dea3f488aad570c
LXT0 <768419921@qq.com>
2020-11-24 19:11:10 +0800

    add barsimple

--- src/components/graphxTest.vue
+++ src/components/graphxTest.vue
@@ -4,14 +4,16 @@
     <div class="content">
       <div id="graphTest" :style="{width: '50%', height: '500px'}"></div>
       <div id="graphTest2" :style="{width: '50%', height: '500px'}"></div>
-      <div :stylr="{width: '50%' , height: '500px'}" style="padding-left:150px;margin-top: 10px">
-        <Table border id="tableImpactUser1" height="470" width="600" :columns="table1.columns1"
+      <div :stylr="{width: '50%' , height: '400px'}" style="padding-left:150px;margin-top: 10px">
+        <Table border id="tableImpactUser1" height="400" width="700" :columns="table1.columns1"
                :data="table1.data1"></Table>
       </div>
-      <div :stylr="{width: '50%' , height: '500px'}" style="padding-left:150px;margin-top: 10px">
-        <Table border id="tableImpactUser2" height="470" width="600" :columns="table2.columns1"
+      <div :stylr="{width: '50%' , height: '400px'}" style="padding-left:150px;margin-top: 10px">
+        <Table border id="tableImpactUser2" height="400" width="700" :columns="table2.columns1"
                :data="table2.data1"></Table>
       </div>
+      <div id="barSimple1" :style="{width: '50%', height: '400px'}"></div>
+      <div id="barSimple2" :style="{width: '50%', height: '400px'}"></div>
     </div>
   </div>
 </template>
@@ -23,6 +25,18 @@ export default {
   name: 'graphxTest',
   data() {
     return {
+      barSimple1: {
+        chart: null,
+        option: null,
+        category: [],
+        data: []
+      },
+      barSimple2:{
+        chart: null,
+        option: null,
+        category: [],
+        data: []
+      },
       graphTest: {
         chart: null,
         option: null,
@@ -84,6 +98,8 @@ export default {
   },
   mounted() {
     this.createGraph()
+    this.createBarSimple1()
+    this.createBarSimple2()
   },
   methods: {
     start() {
@@ -104,11 +120,132 @@ export default {
         this.graphTest2.chart.setOption(this.graphTest2.option, true);
       }
 
-
+    },
+    createBarSimple1(){
+      let dom = document.getElementById("barSimple1")
+      this.barSimple1.chart = this.$echarts.init(dom)
+      this.barSimple1.option = {
+        color: ['#1c8941'],
+        // title: "barChartHomeTeam",
+        tooltip: {
+          show: true
+        },
+        legend: {
+          data: ['数量']
+        },
+        xAxis: {
+          type: 'category',
+          data: this.barSimple1.category,
+          //data:['lixiaot'],
+          axisLabel: {
+            interval: 0, //坐标刻度之间的显示间隔，默认就可以了（默认是不重叠）
+            rotate: 38   //调整数值改变倾斜的幅度（范围-90到90）
+          },
+          grid: {
+            left: '18%',
+            bottom: '38%'
+          }
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: 'VALUE',
+            type: 'bar',
+            data: this.barSimple1.data,
+            itemStyle: {
+              normal: {
+                label: {
+                  show: true,  //开启显示
+                  position: 'top',  //在上方显示
+                  textStyle: {  //数值样式
+                    color: 'black',
+                    fontSize: 16
+                  }
+                }
+              }
+            }
+          }
+        ]
+      }
+      if (this.barSimple1.option && typeof this.barSimple1.option === "object") {
+        this.barSimple1.chart.setOption(this.barSimple1.option, true);
+      }
+    },
+    createBarSimple2(){
+      let dom = document.getElementById("barSimple2")
+      this.barSimple2.chart = this.$echarts.init(dom)
+      this.barSimple2.option = {
+        color: ['#1c8941'],
+        // title: "barChartHomeTeam",
+        tooltip: {
+          show: true
+        },
+        legend: {
+          data: ['数量']
+        },
+        xAxis: {
+          type: 'category',
+          data: this.barSimple2.category,
+          axisLabel: {
+            interval: 0, //坐标刻度之间的显示间隔，默认就可以了（默认是不重叠）
+            rotate: 38   //调整数值改变倾斜的幅度（范围-90到90）
+          },
+          grid: {
+            left: '18%',
+            bottom: '38%'
+          }
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: 'VALUE',
+            type: 'bar',
+            data: this.barSimple2.data,
+            itemStyle: {
+              normal: {
+                label: {
+                  show: true,  //开启显示
+                  position: 'top',  //在上方显示
+                  textStyle: {  //数值样式
+                    color: 'black',
+                    fontSize: 16
+                  }
+                }
+              }
+            }
+          }
+        ]
+      }
+      if (this.barSimple2.option && typeof this.barSimple2.option === "object") {
+        this.barSimple2.chart.setOption(this.barSimple2.option, true);
+      }
     },
     addData() {
-      let that = this;
-      that.updateData()
+      this.updateData()
+      this.barSimple1.chart.setOption({
+        xAxis: {
+          type: 'category',
+          data: this.barSimple1.category
+        },
+        series: [{
+          name: 'VALUE',
+          data: this.barSimple1.data
+        }]
+      })
+      this.barSimple2.chart.setOption({
+        xAxis: {
+          type: 'category',
+          data: this.barSimple2.category
+        },
+        series: [{
+          name: 'VALUE',
+          data: this.barSimple2.data
+        }]
+      })
     },
     updateData() {
       this.$axios.get('/download/graphxdata/subGraph1.gexf')
@@ -327,11 +464,13 @@ export default {
 
       this.$axios.get('/getList/getGraphxData')
         .then((response) => {
+          //console.log(response.data.data)
+
           let array = []
           this.table1.data1 = [];
           array = JSON.parse(JSON.stringify(response.data.data.userResult1Array1))
           for (let i = 0; i < array.length; i++) {
-            console.log(array[i])
+            //console.log(array[i])
             const obj = { // 关键！ 创建一个新对象
               rank: array[i].id.$numberLong,
               name:  array[i].name,
@@ -345,7 +484,7 @@ export default {
           this.table2.data1 = [];
           array2 = JSON.parse(JSON.stringify(response.data.data.userResult1Array2))
           for (let i = 0; i < array2.length; i++) {
-            console.log(array2[i])
+            //console.log(array2[i])
             const obj = { // 关键！ 创建一个新对象
               rank: array2[i].id.$numberLong,
               name:  array2[i].name,
@@ -353,6 +492,29 @@ export default {
               liketeam:  array2[i].team
             }
             this.table2.data1.push(obj)
+          }
+
+          let array3 = []
+          //console.log(response.data.data)
+          this.barSimple1.category = [];
+          this.barSimple1.data = [];
+          array3 = JSON.parse(JSON.stringify(response.data.data.graphResultArray))
+          for (let i = 0; i < array3.length; i++) {
+            //console.log(array3[i].team + array3[i].count)
+            this.barSimple1.category.push(array3[i].team)
+            this.barSimple1.data.push(array3[i].count)
+          }
+
+          let array4 = []
+          this.barSimple2.category = [];
+          this.barSimple2.data = [];
+          array4 = JSON.parse(JSON.stringify(response.data.data.graphResult2Array))
+
+          //console.log("buuu")
+          for (let i = 0; i < array4.length; i++) {
+            //console.log(array3[i].team + array3[i].count)
+            this.barSimple2.category.push(array4[i].name)
+            this.barSimple2.data.push(array4[i].count)
           }
         })
         .catch(function (error) {
